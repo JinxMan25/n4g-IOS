0aed929bbb73c375b376cf77cbfad5691ab78e4f
clanofnoobs <samiulg3@gmail.com>
2014-09-13 04:57:14 -0700

    added table view controller

---
+++ TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  n4g
+//
+//  Created by Josh Wiliwonka on 9/13/14.
+//  Copyright (c) 2014 Josh Wiliwonka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
