cbcb8b166cf7ad8ece693d66e9dc2e32d24e9b8a
clanofnoobs <samiulg3@gmail.com>
2014-09-13 04:52:03 -0700

    init

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  n4g
+//
+//  Created by Josh Wiliwonka on 9/13/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  n4g
+//
+//  Created by Josh Wiliwonka on 9/13/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
