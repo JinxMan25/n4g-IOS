bb383f689787ea0c82b4bf123ea0e9a0a4c13364
clanofnoobs <samiulg3@gmail.com>
2014-09-18 08:24:52 -0700

    thumbnail dimension change

--- TableViewController.m
+++ TableViewController.m
@@ -114,7 +114,7 @@
     NSString *image_url = [tempDictionary objectForKey:@"image_url"];
     //[cell.imageView setImageWithURL:[NSURL URLWithString:image_url]];
     cell.imageView.contentMode = UIViewContentModeScaleAspectFit;
-    UIImageView *myImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,89,89)];
+    UIImageView *myImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,72,72)];
     myImageView.tag = 1;
     [myImageView setImageWithURL:[NSURL URLWithString:image_url]];
 
