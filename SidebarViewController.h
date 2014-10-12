d2900bc889fd697b50b8f7c4f23a930ffc947599
clanofnoobs <samiulg3@gmail.com>
2014-10-12 00:19:39 -0400

    subclass another tableview

---
+++ SidebarViewController.h
@@ -0,0 +1,13 @@
+//
+//  SidebarViewController.h
+//  n4g
+//
+//  Created by Josh Wiliwonka on 10/12/14.
+//  Copyright (c) 2014 Josh Wiliwonka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SidebarViewController : UITableViewController
+
+@end
