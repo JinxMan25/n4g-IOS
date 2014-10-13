f171a03dd339f99bb2c3551ba004c1b761f71dc7
clanofnoobs <samiulg3@gmail.com>
2014-10-13 16:54:28 -0400

    set side menu bar up

---
+++ SideMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  SideMenuViewController.h
+//  n4g
+//
+//  Created by Josh Wiliwonka on 10/13/14.
+//  Copyright (c) 2014 Josh Wiliwonka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SideMenuViewController : UITableViewController
+
+@end
